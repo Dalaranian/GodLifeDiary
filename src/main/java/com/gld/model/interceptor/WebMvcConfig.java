481f7b98fce28af43f3a6d7c2df56348f76ceb39
J.M.Bae <bejm1997@gmail.com>
2023-04-29 20:52:08 +0900

    Update WebMvcConfig.java
    인터셉터에 카카오 컨트롤러 로그인체크 제외

--- src/main/java/com/gld/model/interceptor/WebMvcConfig.java
+++ src/main/java/com/gld/model/interceptor/WebMvcConfig.java
@@ -10,7 +10,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginInterceptor())
                 .addPathPatterns("/**")  
-                .excludePathPatterns("/challenge/main","/login/**","/css/**", "/js/**", "/images/**","/","/resources/**","/index/**"); 
+                .excludePathPatterns("/challenge/main","/login/**","/css/**", "/js/**", "/images/**","/","/resources/**","/index/**", "/kakaoAuth/**"); 
     }
 }
 
