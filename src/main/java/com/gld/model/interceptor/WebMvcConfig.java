f58fb31fa0c2b91ebba13119a06004ecd8d45677
J.M.Bae <bejm1997@gmail.com>
2023-04-30 00:25:59 +0900

    update WebMvcConfig
    인터셉터 예외 추가 ("/error")

--- src/main/java/com/gld/model/interceptor/WebMvcConfig.java
+++ src/main/java/com/gld/model/interceptor/WebMvcConfig.java
@@ -10,7 +10,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginInterceptor())
                 .addPathPatterns("/**")  
-                .excludePathPatterns("/challenge/main","/login/**","/css/**", "/js/**", "/images/**","/","/resources/**","/index/**", "/kakaoAuth/**"); 
+                .excludePathPatterns("/challenge/main","/login/**","/css/**", "/js/**", "/images/**","/","/resources/**","/index/**", "/kakaoAuth/**", "/error"); 
     }
 }
 
