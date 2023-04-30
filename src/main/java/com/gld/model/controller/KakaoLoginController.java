02ebbf3a1d6265a5429f016f8f54bda63e362b99
J.M.Bae <bejm1997@gmail.com>
2023-04-30 15:15:03 +0900

    Update KakaoLoginController.java
    오탈자 수정, 코드 가독성 향상

--- src/main/java/com/gld/model/controller/KakaoLoginController.java
+++ src/main/java/com/gld/model/controller/KakaoLoginController.java
@@ -12,12 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gld.model.biz.LoginBiz;
 import com.gld.model.dto.UserDto;
 
@@ -33,19 +29,19 @@ public class KakaoLoginController {
 	public Map<String, String> kakaoAuth(@RequestBody UserDto dto, HttpSession session) {
 		Map<String, String> result = new HashMap<>();
 		UserDto user = loginBiz.findByUserId(dto.getUserId());
-		System.out.println("kakaoAuth\n"+dto + "\n" + user);
-		if(user != null) {
-			//있는 계정일 시, 바로 로그
+		System.out.println("kakaoAuth\n" + dto + "\n" + user);
+		if (user != null) {
+			// 있는 계정일 시, 바로 로그
 			result.put("result", "true");
-		}else {
-			//없는 계정일 시, 회원가입 페이지로 이동
+		} else {
+			// 없는 계정일 시, 회원가입 페이지로 이동
 			result.put("result", "false");
 			result.put("userId", dto.getUserId());
 			result.put("userName", dto.getUserName());
 		}
 		return result;
 	}
-	
+
 	@GetMapping("/kakaoJoin")
 	public String kakaoJoin(String userId, String userName, Model model) {
 		System.out.println(userId + "\n" + userName);
