1a704c1729cbbefe18e4c418eb6eda09db1d2f66
J.M.Bae <bejm1997@gmail.com>
2023-04-30 22:38:29 +0900

    Update KakaoLoginBiz.java

--- src/main/java/com/gld/model/biz/KakaoLoginBiz.java
+++ src/main/java/com/gld/model/biz/KakaoLoginBiz.java
@@ -1,8 +1,20 @@
 package com.gld.model.biz;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.gld.model.dto.UserDto;
+import com.gld.model.mapper.KakaoLoginMapper;
 
 @Service
 public class KakaoLoginBiz {
+	
+	@Autowired
+	private KakaoLoginMapper kakao;
+
+	public int insert(UserDto dto) {
+		// TODO Auto-generated method stub
+		return kakao.insertUser(dto);
+	}
 
 }
